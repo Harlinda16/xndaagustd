09007c72a40a3fcb2dcbd1ba44f556091a5c0e6e
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-10-09 18:32:28 +0800

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,12 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Saya, Pusing");
+        System.out.println("Bolehkah Saya Tidur?");
+        System.out.println("@Xndaagudtd");
+    }
+
+}
