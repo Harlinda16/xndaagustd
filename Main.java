628b4f955c948ef2f260f431805343b5fbd1562c
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-10-10 20:57:41 +0800

    Day2

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, word!");
+         
+         int angka = 3;
+         int angka2 = 3;
+         
+         System.out.println(angka*angka2);
+    }
+}
