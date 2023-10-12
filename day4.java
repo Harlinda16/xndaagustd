ec98e7d9cca36a970870d4e74833a7a387afee0a
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-10-12 19:31:51 +0800

    Day_4

---
+++ day4.java
@@ -0,0 +1,27 @@
+public class day4 {
+
+
+
+    public static void main(String[] args) {
+  
+        
+        String nama = "Xndaagustd";
+        String alamat = "Pamboang";
+        int umur = 19;
+        double tinggi = 1.45;
+        char jeniskelamin = 'P';
+        
+        boolean status = true;
+        
+        System.out.println(nama);
+        System.out.println(alamat);
+        System.out.println(umur);
+        System.out.println(tinggi);
+        System.out.println(jeniskelamin);
+        
+        
+              System.out.println(true);
+        
+        
+    }
+}
