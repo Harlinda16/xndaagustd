85376336d9a9e905544d0c3f4efe4dace2d0bb30
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-10-13 18:50:58 +0800

    Day_5

---
+++ Day5.java
@@ -0,0 +1,18 @@
+public class day5 {
+
+
+
+    public static void main(String[] args) {
+      
+      byte h = 12;
+        System.out.println("BYTE");
+        System.out.println("nilai byte h = " + (h + 1));
+        System.out.println("nilai max = " + Byte.MAX_VALUE);
+        System.out.println("nilai min = " + Byte.MIN_VALUE);
+        System.out.println("nilai byte = " + Byte.BYTES + "bytes");
+        System.out.println("nilai byte = " + Byte.SIZE + "bit");
+        
+        
+
+    }
+}
