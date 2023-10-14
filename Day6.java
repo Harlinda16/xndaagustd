00a39ff1773c326d209690e725d8674526421d19
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-10-14 17:39:11 +0800

    Day_6

---
+++ Day6.java
@@ -0,0 +1,24 @@
+public class day6 {
+
+
+
+    public static void main(String[] args) {
+      
+        short a = 4;
+        long b = 4;
+        System.out.println("<short>" + "<long>");
+        System.out.println("nilai short a = " + a);
+        System.out.println("nilai long b = " + b);
+        System.out.println("nilai max Short= " + Short.MAX_VALUE); //nilai max Short
+        System.out.println("niai max Long = " + Long.MAX_VALUE); //nilai max Long
+        System.out.println("nilai min Short = " + Short.MIN_VALUE); //nilai min Short
+        System.out.println("nilai min Long = " + Long.MIN_VALUE);  //nilai min Long
+        System.out.println("besar Short =" + Short.BYTES + "bytes");
+        System.out.println("besar long = " + Long.BYTES + "bytes");
+        System.out.println("besar Short = " + Short.SIZE + "bit");
+        System.out.println("besar long = " + Long.SIZE + "bit");
+        
+        
+
+    }
+}
