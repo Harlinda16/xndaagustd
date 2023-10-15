3aae01d2c6b276a244a68be616347b46661fad8f
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-10-15 23:43:36 +0800

    Day_7

---
+++ day7.java
@@ -0,0 +1,34 @@
+public class day7 {
+    
+
+
+    public static void main(String[] args) {
+        
+        
+        // operasi aritmatika
+        int angka1 = 10;
+        int angka2 = 5;
+        
+        int hasil;
+        
+        // penjumlahan
+        hasil = angka1 + angka2;
+        System.out.println(angka1 + " + " + angka2 + " = " +hasil);
+        
+        // pengurangan
+        hasil = angka1 - angka2;
+        System.out.printf(" %d - %d = %d \n",angka1,angka2,hasil);
+        
+        // perkalian
+        hasil = angka1 * angka2;
+        System.out.printf(" %d * %d = %d \n",angka1,angka2,hasil);
+        
+        // pembagian
+        hasil = angka1 / angka2;
+        System.out.printf(" %d / %d = %d \n",angka1,angka2,hasil);
+        
+        
+        
+      
+    }
+}
