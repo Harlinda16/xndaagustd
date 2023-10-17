7c48a30599ef10ba49a388309795cf0b39a4cc2c
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-10-17 22:54:49 +0800

    Operator penugasan

---
+++ day9.java
@@ -0,0 +1,22 @@
+public class day9 {
+    
+
+    public static void main(String[] args) {
+        //operator penugasan
+        
+        //penugasan operator penjumlah
+        byte a = 6;
+        a +=  10; // a = a + 10;
+        System.out.println("nilai a = " + a);
+        
+        // penugasan operator pengurangan
+        byte b = 50;
+        b -= 25;
+        System.out.println("nilai b = " + b);
+        
+        //penugasan operator perkaliaan
+        byte c = 50;
+        c *= 20;
+        System.out.println("nilai c = " + a);
+    }
+}
