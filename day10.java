9df221161d1bc91bb5894eba4a94450a863e755d
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-10-19 00:24:01 +0800

    Variabel deklarasi

---
+++ day10.java
@@ -0,0 +1,43 @@
+ public class day9 {
+    
+
+    public static void main(String[] args) {
+        // kita akan membuat variabel
+        // tipe data
+        int x = 5; // assigment
+        System.out.println("nilai x = " + x );
+        
+        x = 10;
+        System.out.println("nilai x baru = " + x);
+        
+        // kita akan membuat sebuah deklarasi
+        byte angka; // deklarasi
+        
+        angka = 8; 
+        System.out.println("nilai angka = " + angka);
+        
+        short variabel;
+        
+        variabel = 15;
+        System.out.println("nilai variabel = " + variabel);
+        
+        float a;
+        
+        a = 2;
+        System.out.println("nilai a = " + a);
+        
+        double b;
+        
+        b = 12;
+        System.out.println("nilai b = " + b );
+        
+        
+        char keterangan;
+        
+        keterangan = 'a';
+        System.out.println("nilai keterangan = " + keterangan);
+        
+        
+        
+    }
+}
