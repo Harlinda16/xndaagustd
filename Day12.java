4260f371864e3565b43ef0859d9376033e8240b0
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-10-21 05:44:49 +0800

    Nilai genap/ganjil

---
+++ Day12.java
@@ -0,0 +1,16 @@
+public class Day12 {
+
+    public static void main(String[] args) {
+        
+        int[] nilai = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (int i = 0; i < nilai.length; i++) {
+            
+            if (nilai[i] % 2 == 0) {
+                System.out.println(nilai[i] + " adalah angka genap");
+            } else {
+                System.out.println(nilai[i] + " adalah angka ganjil");
+            }
+        }
+    }
+}
