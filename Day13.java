bc0944c8a57556ca71c6bf864e26fbc07b637366
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-10-22 00:12:23 +0800

    Add files via upload

---
+++ Day13.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Day12 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("harga pc gaming : ");
+        int harga = Integer.parseInt(input.nextLine());
+
+        System.out.print("masukkan penghasilan sambo dalam perbulan  : ");
+        int penghasilan = Integer.parseInt(input.nextLine());
+
+        System.out.print("masukkan maksimal uang yang dapat ditabung oleh sambo dalam/bulan : ");
+        int tabungan = Integer.parseInt(input.nextLine());
+
+        System.out.print("masukkan tagihan rumah/bulan : ");
+        int tagihanrumah = Integer.parseInt(input.nextLine());
+
+        System.out.print("masukkan biaya pengeluaran anak pertama dalam/bulan : ");
+        int biayaanakpertama = Integer.parseInt(input.nextLine());
+
+        System.out.print("masukkan biaya 2 anak lainnya dalam/perbulan : ");
+        int biaya2anak = Integer.parseInt(input.nextLine());
+
+        // hitung sisa uang untuk kebutuhan sehari-hari yang diberikan pada istrinya
+        int sisauang = penghasilan - tagihanrumah - biayaanakpertama - (2 * biaya2anak);
+
+        // hitung berapa tahun sambo menabung untuk bisa membeli pc gaming
+        int tahundibutuhkan = harga / (tabungan * 12); // Assuming tabungan is in per month
+        System.out.print("Sambo memerlukan sekitar " + tahundibutuhkan + " tahun atau lebih untuk bisa membeli pc gaming");
+
+    }
+}
