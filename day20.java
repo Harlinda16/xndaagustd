fc4fb47cdf266cf4f7f01609a9ee214c19ccde34
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-10-28 22:52:01 +0800

    Day_20

---
+++ day20.java
@@ -0,0 +1,27 @@
+public class day20  {
+
+    public static void main(String[] args) {
+        // Ini dengan list 
+        
+        System.out.println("___PROGRAM PERTAMA___");
+        int[] angka_list = {0, 1, 3, 5, 7, 9}; 
+        for (int x : angka_list) {
+            System.out.println("x sekarang -> " + x); 
+        }
+        System.out.println("akhir dari program 1\n");
+
+        // Ini dengan for loop
+        
+        System.out.println("___PROGRAM KEDUA___");
+        for (int x = 1; x < 11; x++) { 
+            System.out.println("x sekarang -> " + x);
+        }
+        System.out.println("akhir dari program 2");
+    }
+}
+        
+        
+        
+    
+  
+    
