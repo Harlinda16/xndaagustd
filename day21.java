28a988202d9dbea5e881d1e2bcc64fcb6cb6240c
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-10-30 00:08:51 +0800

    Update day21.java

--- day21.java
+++ day21.java
@@ -1,21 +1,10 @@
-import java.util.Scanner;
-
-public class day21  {
-
+public class Day21 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         
-        System.out.print("masukkan nilai panjang : ");
-        double panjang = input.nextDouble();
-        
-        System.out.print("masukkan nilai lebar : ");
-        double lebar = input.nextDouble();
-        
-        
-        double luas = panjang * lebar;
-        System.out.print("luas persegi panjang adalah : " + luas );
-        
-        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println( i + " Nama saya adalah Harlinda");
+            
+            
+        }
     }
 }
-        
