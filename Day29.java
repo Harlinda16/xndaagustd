42fe78d0c86b727eedb436a520895a72b61ba7ee
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-11-07 00:16:19 +0800

    Day29_total harga lahan_

--- Day29.java
+++ Day29.java
@@ -1,17 +1,19 @@
-public class Day29  {
+public class day29 {
     
     public static void main(String[] args) {
         
-         int panjang = 10;
-         int lebar = 15;
-        int hargatanahpermeterpersegi = 5000000;
+         double panjang = 10;
+         double lebar = 15;
+         double kampling = 30;
+        double hargatanahpermeterpersegi = 500000;
         
-        int luas = panjang * lebar;
-        int totalharga = luas * hargatanahpermeterpersegi;
+        double luas = panjang * lebar;
+        double totalLahan = luas * kampling;
+        double harga = totalLahan*hargatanahpermeterpersegi;
         
         
         System.out.println("Luas lahan : " + luas + " meter persegi");
         System.out.println("harga permeter persegi : Rp. " +hargatanahpermeterpersegi);
-        System.out.println("total harga lahan : Rp. " + totalharga);
+        System.out.println("total harga lahan : Rp. " + harga);
     }
 }
