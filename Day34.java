6e4bde1eca8a47cc55350cb56822317bf5afab6a
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-11-11 23:18:49 +0800

    Day34 _(Pengulangan)

---
+++ Day34.java
@@ -0,0 +1,14 @@
+public class Day34  {
+    
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 20; i++) {
+            if (i % 3 == 0) {
+                
+                // Jika angka adalah kelipatan 3, tidak ada output.
+            } else {
+                System.out.println("Angka " + i + " bukan kelipatan 3");
+            }
+        }
+    }
+}
