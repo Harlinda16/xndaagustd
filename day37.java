1b6185fdb63ab46fa87050e93853779f28245924
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-11-14 22:54:32 +0800

    Day_37 (Array)

---
+++ day37.java
@@ -0,0 +1,33 @@
+public class day37 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("__Assignment Array__");
+        
+        byte[] arrayByte = {6,7,8,9,10};
+        
+        System.out.println(arrayByte[0]);
+        System.out.println(arrayByte[1]);
+        System.out.println(arrayByte[2]);
+        System.out.println(arrayByte[3]);
+        System.out.println(arrayByte[4]);
+        
+        
+        System.out.println("__Deklarasi Array__");
+        float[] arrayfloat = new float[5];
+        
+        arrayfloat[3] = 11;
+        arrayfloat[4] = 15;
+        
+        
+            System.out.println(arrayfloat[0]);
+            System.out.println(arrayfloat[1]);
+            System.out.println(arrayfloat[2]);
+            System.out.println(arrayfloat[3]);
+            System.out.println(arrayfloat[4]);
+        
+        
+        
+        
+         }
+}
