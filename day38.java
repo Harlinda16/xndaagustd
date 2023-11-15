efe2e522c2076d0508b99febdc18583725c2e608
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-11-15 22:04:04 +0800

    Day_38 (angkaArray)

---
+++ day38.java
@@ -0,0 +1,21 @@
+public class day38  {
+    
+    public static void main(String[] args) {
+
+        int[] angkaArray = new int[5]; 
+        
+        angkaArray[0] = 10; 
+        angkaArray[1] = 20; 
+        angkaArray[2] = 30; 
+        angkaArray[3] = 40; 
+        angkaArray[4] = 50;
+        
+        System.out.println("Isi array:"); 
+        
+        for (int i = 0; i < angkaArray.length; i++) { 
+            
+            System.out.println(angkaArray[i]);
+            
+        } 
+    } 
+}
