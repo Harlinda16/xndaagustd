c228680567090dba0e85b802e33896d1fcbad995
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-11-27 21:06:33 +0800

    Day_50

---
+++ day50.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class day50  {
+     
+    public static void main(String[] args) {
+        
+        System.out.println("====Contoh nama======");
+        String nama[] = new String[5];
+        
+        System.out.println("");
+        
+        nama[0] = "Harlinda";
+        nama[1] = "Xndaagustd";
+        nama[2] = "D'day";
+        
+        System.out.println(nama[0]);
+        System.out.println(nama[1]);
+        System.out.println(nama[2]);
+        
+        System.out.println(" ");
+        
+        System.out.println("========Contoh angka=========");
+        
+        System.out.println("");
+        
+        int nilai[] = {22, 33, 44, 55};
+        
+        System.out.println("Cetak nilai elemen 0 = "+ nilai[0]);
+        System.out.println("Cetak nilai elemen 1 = "+ nilai[1]);
+        System.out.println("Cetak nilai elemen 2 = "+ nilai[2]);
+        System.out.println("Cetak nilai elemen 3 = "+ nilai[3]);
+        
+    
+   }
+ }
