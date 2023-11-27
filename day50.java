b7de89e695837df88d4aea0c7e4e0558ce22cf45
Harlinda16 <146844313+Harlinda16@users.noreply.github.com>
2023-11-27 22:05:17 +0800

    day50_Array(inputan Scanner)

--- day50.java
+++ day50.java
@@ -1,34 +1,25 @@
 import java.util.Scanner;
-public class day50  {
-     
+
+public class day50 {
     public static void main(String[] args) {
-        
-        System.out.println("====Contoh nama======");
-        String nama[] = new String[5];
-        
-        System.out.println("");
-        
-        nama[0] = "Harlinda";
-        nama[1] = "Xndaagustd";
-        nama[2] = "D'day";
-        
-        System.out.println(nama[0]);
-        System.out.println(nama[1]);
-        System.out.println(nama[2]);
-        
-        System.out.println(" ");
-        
-        System.out.println("========Contoh angka=========");
-        
-        System.out.println("");
-        
-        int nilai[] = {22, 33, 44, 55};
-        
-        System.out.println("Cetak nilai elemen 0 = "+ nilai[0]);
-        System.out.println("Cetak nilai elemen 1 = "+ nilai[1]);
-        System.out.println("Cetak nilai elemen 2 = "+ nilai[2]);
-        System.out.println("Cetak nilai elemen 3 = "+ nilai[3]);
-        
-    
-   }
- }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang array: ");
+        int panjangArray = scanner.nextInt();
+
+        int[] angka = new int[panjangArray];
+
+        System.out.println("Masukkan nilai-nilai ke dalam array:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.print("Nilai index ke-" + i + ": ");
+            angka[i] = scanner.nextInt();
+        }
+
+        System.out.println("Isi array:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Nilai index ke-" + i + " adalah " + angka[i]);
+        }
+
+        scanner.close();
+    }
+}
